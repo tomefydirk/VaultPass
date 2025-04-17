3fc5900d08bb2393092f8f8cd82b5ec7d817d672
tomefydirk <tomefydirk@yahoo.com>
2025-04-17 14:53:42 +0300

    create mod vaultpass

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
-fn main() {
-    println!("Hello, world!");
+mod vaultpass;
+use vaultpass::run_vaultpass;
+fn main() -> Result<(), eframe::Error> {
+    run_vaultpass()
 }
---
+++ src/vaultpass.rs
@@ -0,0 +1,41 @@
+use std::{alloc::System, fmt::format};
+
+use eframe::{egui::{self,Button, CentralPanel, Color32, Context, Frame, Pos2, Rect, Stroke, TextEdit, Ui,Vec2}};
+ #[derive(Default)]
+pub struct Vaultpass{
+    pub nom_obtained:String,
+    pub pass_obtained:String
+
+}
+impl Vaultpass{
+     pub fn new(_cc:&eframe::CreationContext<'_>)->Self{
+        Default::default()
+    }
+}
+fn add_label_field(ui:&mut Ui,text:String,saisie:&mut String){
+        ui.horizontal(|ui1|{
+            ui1.label(text);
+            ui1.text_edit_singleline(saisie);
+        });
+}
+impl eframe::App for Vaultpass{
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        CentralPanel::default().show(ctx, |ui|{
+           ui.vertical(|ui|{
+            add_label_field(ui,format!("{}","Nom") , &mut self.nom_obtained);
+            add_label_field(ui,format!("{}","Mot de passe") , &mut self.pass_obtained);
+            if ui.button("Soummettre").clicked(){
+                
+            }
+           }
+           )
+        });
+    }
+}
+pub fn run_vaultpass() -> Result<(), eframe::Error> {
+    eframe::run_native(
+        "Vaultpass",
+        eframe::NativeOptions::default(),
+        Box::new(|cc| Ok(Box::new(Vaultpass::new(cc))))
+    )
+}
